527c438321cba12373a030ccdf6fca283b797f77
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-05-16 03:54:10 +0200

    minor modifications

--- Scripts/Download_fastq_from_geo.py
+++ Scripts/Download_fastq_from_geo.py
@@ -31,42 +31,36 @@ def geo_id_to_srx_ids(geo_id):
     return srx_id_list
 
 
-def download_run(run_id, output_directory, file_name=None):
+def download_run(run_id, output_directory):
     result_json = requests.get(
         f'https://www.ebi.ac.uk/ena/portal/api/filereport?accession={run_id}&download=false&fields=fastq_aspera%2Cfastq_md5&format=json&result=read_run').json()
-
-    # No specific filename if there is more than one file to download
-    if len(result_json) > 1:
-        file_name = None
 
     for single_run in result_json:
         fastq_aspera_path = single_run['fastq_aspera']
         fastq_md5checksum = single_run['fastq_md5']
 
-        if not file_name:
-            file_name = os.path.basename(fastq_aspera_path)
-
-        full_path = os.path.join(output_directory, file_name)
-        if not os.path.exists(full_path):
-            os.makedirs(full_path)
-
         cmd = "ascp -QT -l 500m -P33001 {} -i {} era-fasp@{} {}".format(
             "",  # Additional args
             ssh_key_file,
             fastq_aspera_path,
-            full_path)
+            output_directory)
+
         process = subprocess.check_call(cmd, stdout=subprocess.PIPE, shell=True)
 
         print(f'Retval: {process}')
 
         # Check md5sum
+        full_path = os.path.join(output_directory, os.path.basename(fastq_aspera_path))
         process = subprocess.Popen(['md5sum', full_path],
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
+
         stdout, _ = process.communicate()
 
         if stdout.decode('UTF-8').split(' ')[0] != fastq_md5checksum:
             return False
+
+    return True
 
 
 def download_file(input_csv, base_path=''):
@@ -90,32 +84,29 @@ def download_file(input_csv, base_path=''):
         # Parse row details
         geo_id = row['GEO #']
         file_id = row['file #']
-        file_name = row['file name']
 
         # Create output paths
         output_path = os.path.join(base_path, geo_id, file_id)
-        file_name = f'{file_name}.fastq.gz'
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
 
         # Get run details from GEO ID
-        srx_id = geo_id_to_srx_ids(file_id)[0]
-        print(f'{file_id} is associated with the following ID(s): {srx_id}')
+        srx_id_list = geo_id_to_srx_ids(file_id)
+        print(f'{file_id} is associated with the following ID(s): {srx_id_list}')
 
-        # # Write run information to file:
-        # with open(os.path.join(output_path, f'{file_name}.txt'), 'w') as file:
-        #     file.writelines(run_text)
+        for srx_id in srx_id_list:
 
-        # Download file, if unsuccessful add tag
-        print(f'Start downloading files...')
-        valid_file = download_run(srx_id, output_directory=output_path, file_name=file_name)
-
-        if valid_file:
-            input_df.loc[index, 'finished'] = True
+            # Download file, if unsuccessful add tag
+            print(f'Start downloading files...')
+            if download_run(srx_id, output_directory=output_path):
+                input_df.loc[index, 'finished'] = True
+            else:
+                print("Download file failed")
+                continue
 
         input_df.to_csv(input_csv + '.out')
-
         print('Done')
 
 
 if __name__ == '__main__':
     download_file('GEO data list.csv', '/localscratch/kyuan')
-
