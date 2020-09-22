844e5c051422711d9457c17572764ba7b4ccb9ee
Kevin Yuan <kevihiiin@users.noreply.github.com>
2020-09-22 15:59:16 +0200

    Cleanup and remove unused files

--- Analysis/TEPIC/auroc-example.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn.metrics import roc_auc_score
-
-y_true = np.array([0, 0, 1, 1])
-y_scores = np.array([1, 1, 0.35, 0.8])
-
-result = roc_auc_score(y_true, y_scores)
-print(result)
--- PA_Scripts/Download_Data/subscript.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-cmd = "ascp -l 1000m -P33001 -i /home/kevin/.aspera/cli/etc/asperaweb_id_dsa.openssh era-fasp@fasp.sra.ebi.ac.uk:/vol1/fastq/SRR726/004/SRR7265524/SRR7265524_1.fastq.gz /tmp/GSM3176759.fastq.gz"
-process = subprocess.check_call(cmd,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                shell=True)
-
